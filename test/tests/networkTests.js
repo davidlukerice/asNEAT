bf0fe76e6ffa4bb879a03fe18c77b28a8700af8d
David Rice <davidlukerice@gmail.com>
2014-05-25 12:31:01 -0500

    few more crossing tests

--- test/tests/networkTests.js
+++ test/tests/networkTests.js
@@ -13,14 +13,34 @@ test("Same default objects/arrays not referenced by multiple networks", function
 });
 
 test("crossWith", function() {
-  var a = new Network(),
-      b = new Network(),
-      c = a.crossWith(b);
+  var a,b,c;
+  
+  a = new Network();
+  c = a.crossWith(a);
+  ok(a.nodes.length === c.nodes.length, 
+    "crossing with itself contains same number nodes");
+  ok(a.connections.length === c.connections.length, 
+    "crossing with itself contains same number nodes");
+
+  a = new Network(),
+  b = new Network(),
+  c = a.crossWith(b);
   equal(c.nodes.length, a.nodes.length+b.nodes.length-1,
     "Child has same number nodes, less the output");
   equal(c.connections.length, a.connections.length+b.connections.length,
     "Child has same number connections");
 
+  a = new Network();
+  b = a.clone();
+  a.splitMutation();
+  b.addOscillator();
+  c = a.crossWith(b);
+  ok(c.nodes.length === (a.nodes.length+1) &&
+     c.nodes.length === (b.nodes.length+1),
+    "SplitMutation && addOscillator adds correct number nodes");
+  ok(c.connections.length === (a.connections.length+1) &&
+     c.connections.length === (b.connections.length+2),
+    "SplitMutation && addOscillator adds correct number connections");
 });
 
 test("split node", function() {
